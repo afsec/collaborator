4a9069d6f2b0d5fc4c0a37fce28ba17d507fc041
Antonio Feitosa <antonio.cfc@gmail.com>
2020-07-08 18:14:46 -0300

    Fix

--- prj01/src/main.rs
+++ prj01/src/main.rs
@@ -1,13 +1,15 @@
 #![warn(clippy::all)]
 
-// use std::str::FromStr;
 use async_std::io;
 use async_std::net::{TcpListener, TcpStream};
 use async_std::prelude::*;
 use async_std::task;
 
+use chrono::{DateTime, Utc};
+use std::time::Instant;
+
 const MAX_BYTES: usize = 10_485_760; // 10 MBytes
-// const MAX_BYTES: usize = 20; // 10 MBytes
+                                     // const MAX_BYTES: usize = 20; // 10 MBytes
 
 #[async_std::main]
 async fn main() -> io::Result<()> {
@@ -31,20 +33,50 @@ async fn main() -> io::Result<()> {
 
 // Take a TCP stream, and convert it into sequential HTTP request / response pairs.
 async fn accept(mut stream: TcpStream) -> io::Result<()> {
-    // let mut stream = stream.clone();
-    let mut recv = vec![0u8; MAX_BYTES];
-    let recv_length = stream.read_to_end(&mut recv).await?;
-    let mut data = String::new();
-    for c in recv {
+    let start = Instant::now();
+    let mut buf = vec![0u8; MAX_BYTES];
+    let recv_length = stream.read(&mut buf).await?;
+    let mut recv_data = String::new();
+    for c in buf {
         if c > 1 {
-            data.push(c as char);
+            recv_data.push(c as char);
         }
     }
-    // dbg!(recv);
-    // let raw_http_request = String::from_utf8(recv);
-    println!("starting new connection from {}", stream.peer_addr()?);
-    println!("Bytes received: {}", recv_length);
-    println!("{}",data);
-    // dbg!(raw_http_request);
+    let now: DateTime<Utc> = Utc::now();
+
+    let body = format!(r#"<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Directory listing for /</title>
+    </head>
+    <body>
+        <h1>Directory listing for /</h1>
+        <hr>
+        <p>{}</p>
+    </body>
+</html>"#,now);
+
+    let now: DateTime<Utc> = Utc::now();
+    let headers = format!("HTTP/1.0 200 OK\r\nServer: nginx\r\nDate: {now}\r\nContent-type: text/html; charset=utf-8\r\nContent-Length: {body_len}\r\n\r\n", now=now,body_len=body.len());
+    let response = format!("{}{}", headers, body);
+
+    stream
+        // .write_all(include_bytes!("../../response-edited.raw"))
+        .write_all(response.as_bytes())
+        .await?;
+    let duration = start.elapsed();
+
+    println!(
+        "-------[ {} -> {} ({} Bytes - elapsed {:?} ]-------",
+        stream.peer_addr()?,
+        stream.local_addr()?,
+        recv_length,
+        duration,
+    );
+    // TODO: Create Struct and Store recv_data into File or Database
+    println!("{}", recv_data);
+    println!("{}", response);
+    println!();
     Ok(())
 }
